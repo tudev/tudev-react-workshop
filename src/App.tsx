eaef1f7dcc334bbb8dc1a3649ba40538c1eabb58
samirbuch <samir.buch@temple.edu>
2025-10-26 13:42:20 -0400

    add tudev logo stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
+import tudevLogo from './assets/tudev.jpeg'
 import './App.css'
 
 function App() {
@@ -15,8 +16,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://tudev.org/" target="_blank">
+          <img src={tudevLogo} className="logo tudev" alt="TUDev logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TUDev</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +30,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite, React, or TUDev logos to learn more
       </p>
     </>
   )
